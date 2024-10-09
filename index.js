2828a4d1f6978538ae9427fe932c70dc371827b8
Matthew Torma <tormamatthew@gmail.com>
2024-10-09 13:01:19 -0500

    Add Total style and js

---
+++ index.js
@@ -0,0 +1,18 @@
+//Create string  You have received this message because
+// you have been chosen to open an important vault.
+//Here is the secret combination:
+
+let Text = `You have received this message because you have been chosen to open an important vault.
+  Here is the seceret combination:`;
+//cont functions for 10-40-39, 3 diff operations to display in seperate alert box
+
+let OCode = 50 / 5;
+let TCode = 20 * 2;
+let HCode = 20 + 19;
+
+console.log(Text + `` + OCode + TCode + HCode);
+//Create a Alert Box containing Text and Numbers
+
+alert(Text +` `+ OCode +` ` + TCode +` ` + HCode)
+
+//push to git
